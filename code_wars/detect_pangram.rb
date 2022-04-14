6dc70b7d78f7503ed2700ea6acbb3b3f84aa2be8
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-14 11:11:59 -0400

    adding detect_pangram

---
+++ code_wars/detect_pangram.rb
@@ -0,0 +1,24 @@
+=begin
+6 kyu
+
+A pangram is a sentence that contains every single letter of the
+alphabet at least once. For example, the sentence "The quick brown
+fox jumps over the lazy dog" is a pangram, because it uses the
+letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return
+True if it is, False if not. Ignore numbers and punctuation.
+
+=end
+
+ALPHABET = ('a'..'z').to_a
+
+def pangram?(string)
+  ALPHABET.all? do |letter|
+    string.downcase.include?(letter)
+  end
+end
+
+p pangram?("The quick brown fox jumps over the lazy dog.") == true
+p pangram?("This is not a pangram.") == false
+
