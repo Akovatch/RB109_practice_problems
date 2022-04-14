aee3c6b35a61efdab9800fa67b237e129352b715
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-14 16:29:39 -0400

    adding find_the_mine and longest_alphabetical_substring

---
+++ code_wars/longest_alphabetical_substring.rb
@@ -0,0 +1,32 @@
+=begin
+
+6 kyu
+
+Find the longest substring in alphabetical order.
+
+Example: the longest alphabetical substring in "asdfaaaabbbbcttavvfffffdf" is "aaaabbbbctt".
+
+There are tests with strings up to 10 000 characters long so your code will need to be efficient.
+
+The input will only consist of lowercase characters and will be at least one letter long.
+
+If there are multiple solutions, return the one that appears first.
+
+=end
+
+def longest(string)
+  results = ' '
+  string.chars.each do |char|
+    char >= results[-1] ? results << char : results << ' ' + char
+  end
+  results.split.max_by { |substring| substring.length }
+end
+
+p longest('asd') == 'as'
+p longest('nab') == 'ab'
+p longest('abcdeapbcdef') == 'abcde'
+p longest('asdfaaaabbbbcttavvfffffdf') == 'aaaabbbbctt'
+p longest('asdfbyfgiklag') =='fgikl'
+p longest('z') == 'z'
+p longest('zyba') == 'z'
+
