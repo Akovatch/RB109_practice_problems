0988504bce9734dd7f67ff91e601439afa023887
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-19 12:53:38 -0400

    Update anagram_difference.rb

--- code_wars/anagram_difference.rb
+++ code_wars/anagram_difference.rb
@@ -17,24 +17,6 @@ A word is an anagram of another word if they have the same letters (usually in a
 Do not worry about case. All inputs will be lowercase.
 =end
 
-# input: two strings
-# output: an integer (how many letters were removed)
-# rules:
-  # all input will be lowercase
-  # edge cases = can be empty strings
-# alg:
-  # init results array
-  # get original total
-    # get length of each word and add them together, assign to a variable original_size
-  # figure out which letters do both words have in common
-    # iterate through the chars word1
-      # if present in word2,
-        # push char into a results array
-        # substitute letter in word1 with empty space
-      # otherwise, substitute letter in word1 with empty space
-  # multiply the number of elements in results by 2
-  # subtract this from the total number of original_size
-
 def anagram_difference(word1, word2)
   common_chars = []
   original_total = word1.size + word2.size
