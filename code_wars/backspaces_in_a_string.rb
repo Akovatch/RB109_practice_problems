cd8add2aacd63336aebb8a98b577eb6acde61ea3
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-19 13:04:51 -0400

    Update backspaces_in_a_string.rb

--- code_wars/backspaces_in_a_string.rb
+++ code_wars/backspaces_in_a_string.rb
@@ -13,18 +13,24 @@ Examples
 ""              ==>  ""
 =end
 
+# input: string
+# output: string
+# rules:
+  # input or output can be an empty string
+# alg:
+  # init stack variable
+  # iterate over chars (array) of string
+    # if a char is a #, delete the last element of the stack
+    # otherwise, char goes to stack
+  # return the stack joined as a string
+
 def clean_string(string)
-	stack = []
+  stack = []
   string.chars.each do |char|
-    if char == '#'
-      stack.pop
-    else
-      stack << char
-    end
+    char == '#' ? stack.pop : stack << char
   end
   stack.join
 end
 
 p clean_string('abc#d##c') == "ac"
 p clean_string('abc####d##c#') == ""
-
