a75900d11e9e10af462d83977bc5eed42dd44192
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-19 13:56:33 -0400

    Update count_and_group_character_occurrences.rb

--- code_wars/count_and_group_character_occurrences.rb
+++ code_wars/count_and_group_character_occurrences.rb
@@ -11,23 +11,6 @@ get_char_count("cba") => {1=>["a", "b", "c"]}
 You should ignore spaces, special characters and count uppercase letters as lowercase ones.
 =end
 
-# input: string
-# ouput: a hash (keys are integers, values are an array of lowercase letters)
-# rules:
-  # only count digits and letters
-  # uppercase are counted as lowercase
-  # return values in lowercase form
-  # each value array is sorted alphabetically
-# alg:
-  # init results hash
-  # create an alphabet array of lowercase letters
-  # iterate of the letters of the alphabet array
-    # for each letter (lowercase version), get the count of how many times it happens in the string
-      # if the count is greater than 0:
-        # if key doesn't exist in the hash, add key and the value as an array ["g"]
-        # if the key does exist, add the value to the exiting array (<< "g")
-  # sort hash by keys (greatest to smallest)
-
 def get_char_count(string)
   chars = ('0'..'9').to_a + ('a'..'z').to_a
   results = {}
