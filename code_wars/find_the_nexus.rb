80dfd1ef8817c240706b1a0292fefcddfd5c38ec
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-20 10:37:47 -0400

    Update find_the_nexus.rb

--- code_wars/find_the_nexus.rb
+++ code_wars/find_the_nexus.rb
@@ -1,11 +1,11 @@
 =begin
 
-6 kyu - Tricky (make sure to run random tests in CW)
+6 kyu - Tricky (use sorting methods, not pushing to hash)
 
 Not to brag, but I recently became the nexus of the Codewars universe! My honor and my rank
 were the same number. I cried a little.
 
-Complete the method that takes a hash/object/directory/association list of users, and find the
+Complete the method that takes a hash list of users, and find the
 nexus: the user whose rank is the closest is equal to his honor. Return the rank of this user.
 For each user, the key is the rank and the value is the honor.
 
@@ -23,14 +23,21 @@ users = {  1  =>  93,
           30  =>   2 }
 =end
 
-  def nexus(users)
-    users.sort.sort_by { |key, value| (key - value).abs }.first.first
-  end
+# input: hash
+# output: integer representing the key of a pair
+# rules:
+  # If nobody has an exact rank/honor match, return the rank of the user who comes closest.
+  # If there are several users who come closest, return the one with the lowest rank (numeric value).
+  # The hash will not necessarily contain consecutive rank numbers; return the best match from the ranks provided.
+# alg:
+  # sort by hash key (this will ensure that lowest key always is returned first) => nested arrays
+  # sort by the difference between key and value => will return nested arrays?
+  # return the first element of the first pair
 
-  # My original solution worked with the given tests but not the random tests in codewars. The solution was to add #sort before #sort_by, as this
-  # ensured that when a the first item was returned it would already have the the lowest key. Otherwise, the method would return the equal pairs
-  # in the order they first appeared.
+def nexus(hash)
+  hash.sort.sort_by { |array| (array[0] - array[1]).abs }.first.first
+end
 
-p nexus({1 => 3, 3 => 3, 5 => 1}) # == 3
-p nexus({1 => 10, 2 => 6, 3 => 4, 5 => 1}) # == 3
-p nexus({1 => 10, 2 => 3, 3 => 4, 5 => 1}) # == 2
+p nexus({1 => 3, 3 => 3, 5 => 1}) == 3
+p nexus({1 => 10, 2 => 6, 3 => 4, 5 => 1}) == 3
+p nexus({1 => 10, 2 => 3, 3 => 4, 5 => 1}) == 2
