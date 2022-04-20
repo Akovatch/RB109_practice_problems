f9d9b069f13777f715509bdf0e4bd5f851b9d71e
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-20 14:37:10 -0400

    Update practice_problem_4.rb

--- video_problems/practice_problem_4.rb
+++ video_problems/practice_problem_4.rb
@@ -1,7 +1,15 @@
 # Write a method that takes an array of integers and returns the
 # two numbers that are closest together in value.
 
-# Examples:
+def closest_numbers(array)
+  results = []
+  array.each do |num1|
+    array.each do |num2|
+      results << [num1, num2] if num1 != num2
+    end
+  end
+  results.min_by { |array| (array[0] - array[1]).abs }
+end
 
 p closest_numbers([5, 25, 15, 11, 20]) == [15, 11]
 p closest_numbers([19, 25, 32, 4, 27, 16]) == [25, 27]
