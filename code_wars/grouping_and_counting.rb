1c62272d48ad3742da5eb88c4a57ad80a3d6d8b0
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-20 11:03:46 -0400

    Update grouping_and_counting.rb

--- code_wars/grouping_and_counting.rb
+++ code_wars/grouping_and_counting.rb
@@ -16,27 +16,26 @@ The following methods were disabled:
 
 Array#count
 Array#length
+
 =end
 
-# input: array
-# output: hash
+# input: array of integers
+# output: a hash: unique nums are keys, counts as values
 # rules:
-  # empty or nil input returns nil, not a hash
+  # Empty or nil input must return nil instead of a hash.
   # cannot use #count or #length
 # alg:
-  # init results hash
-  # return nil if array is empty or input is nil
-  # iterate over array
-    # if the number does not exist as a key, key = element, value = 1
-    # if the number exists as a key in the hash, increment the pair's value by one
+  # handle nil and empty array input: return nil if...
+  # iterate through array - each_with_object (Hash.new(0))
+    # if object includes num as hash key, increment the value
+    # if it does not, add num as key and increment value
+    # return hash
 
-def group_and_count(input)
-  results = {}
-  return nil if input == nil || input.empty?
-  input.each do |num|
-    results.key?(num) ? results[num] += 1 : results[num] = 1
+def group_and_count(array)
+  return nil if array.nil? || array.empty?
+  array.each_with_object(Hash.new(0)) do |num, hash|
+    hash[num] += 1
   end
-  results
 end
 
 p group_and_count([1,1,2,2,2,3]) == {1=>2, 2=>3, 3=>1}
@@ -44,9 +43,3 @@ p group_and_count([]) == nil
 p group_and_count(nil) == nil
 p group_and_count([1, 7, 5, -1]) == {1=>1, 7=>1, 5=>1, -1=>1}
 
-# Codewars: using Hash.new(0) to initialize hash to values of 0
-
-def group_and_count(input)
-  return nil if input.nil? || input.empty?
-  input.each_with_object(Hash.new(0)){ |k, h| h[k] += 1 }
-end
