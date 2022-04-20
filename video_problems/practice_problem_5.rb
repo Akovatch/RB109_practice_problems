096c01c6f90bd62c3b74e97177571b34f9316ff2
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-20 14:53:35 -0400

    Update practice_problem_5.rb

--- video_problems/practice_problem_5.rb
+++ video_problems/practice_problem_5.rb
@@ -5,12 +5,19 @@
 # string. When counting characters, consider the uppercase and
 # lowercase version to be the same.
 
-# Examples:
+def least_common_char(string)
+  results = {}
+  string.downcase.chars do |char|
+    count = string.downcase.count(char)
+    results.key?(count) ? results[count] << char : results[count] = [char]
+  end
+  results.min[1][0]
+end
 
-p least_common_char("Hello World") #== "h"
-p least_common_char("Peter Piper picked a peck of pickled peppers") #== "t"
-p least_common_char("Mississippi") #== "m"
-p least_common_char("Happy birthday!") #== ' '
-p least_common_char("aaaaaAAAA") #== 'a'
+p least_common_char("Hello World") == "h"
+p least_common_char("Peter Piper picked a peck of pickled peppers") == "t"
+p least_common_char("Mississippi") == "m"
+p least_common_char("Happy birthday!") == ' '
+p least_common_char("aaaaaAAAA") == 'a'
 
 # The tests above should print "true".
