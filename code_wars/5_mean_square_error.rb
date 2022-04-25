d738ee49535918e80737ea3debf7db6a2fe88d00
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-25 19:35:53 -0400

    Create 5_mean_square_error.rb

---
+++ code_wars/5_mean_square_error.rb
@@ -0,0 +1,34 @@
+=begin
+
+5 kyu
+
+Complete the function that
+ - accepts two integer arrays of equal length
+ - compares the value each member in one array to the corresponding member in the other
+ - squares the absolute value difference between those two values
+ - and returns the average of those squared absolute value difference between each member pair.
+
+Examples:
+
+[1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+[10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+[-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+
+# get difference in abs val, square it, add all together, divide by num of elements
+
+=end
+
+def solution (array1, array2)
+  results = []
+  array1.each_with_index do |num, index|
+    results << (array1[index] - array2[index]).abs
+  end
+  results.map { |num| num ** 2}.sum.to_f / array1.size
+end
+
+p solution([1, 2, 3], [4, 5, 6]) == 9
+p solution([10, 20, 10, 2], [10, 25, 5, -2]) == 16.5
+p solution([-1, 0], [0, -1]) == 1
+
+
+
