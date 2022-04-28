30c11f77ca864c7e6a352b0778b1ce2eac4b9bcf
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-28 17:27:29 -0400

    Update 5_convert_pascal_case_to_snake_case.rb

--- code_wars/5_convert_pascal_case_to_snake_case.rb
+++ code_wars/5_convert_pascal_case_to_snake_case.rb
@@ -11,44 +11,29 @@
 # "App7Test"        -->  "app7_test"
 # 1                 -->  "1"
 
+# input: string in pascal case
+# output: string in snake_case
+# rules:
+  # all lowercase, seperated by underscore
+  # numbers should be converted to digits
 
+# algorithm:
+  # init results array
+  # iterate over chars of string (chars array)
+    # push all chars into results array
+      # if char is uppercase, push an "_" into results ahead of the char
+  # join and downcase results array, remove first char
+
+def to_underscore(string)
+  return string.to_s if string == string.to_s.to_i
+  results = []
+  string.chars.each do |char|
+    char =~ /[A-Z]/ ? results << "_" + char.downcase : results << char
+  end
+  results.join[1..-1]
+end
 
 p to_underscore("TestController") ==  "test_controller"
 p to_underscore("MoviesAndBooks") == "movies_and_books"
 p to_underscore("App7Test") == "app7_test"
 p to_underscore(1) == "1"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def to_underscore(string)
-#   results = []
-#   return string.to_s if string.to_s.to_i == string
-#   string.chars.each do |char|
-#     char.downcase == char ? results << char : results << '_' + char.downcase
-#   end
-#   results.join[1..-1]
-# end
