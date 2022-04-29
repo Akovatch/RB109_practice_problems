36b8080a117738405dad18e5a2c6a901981cdf8f
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-29 11:29:23 -0400

    Update 5_sum_of_pairs.rb

--- code_wars/5_sum_of_pairs.rb
+++ code_wars/5_sum_of_pairs.rb
@@ -1,6 +1,6 @@
 # 5 kyu
 
-# Sum of Pairs
+# Sum of Pairs - much harder than it looks bc need to consider index placement of pair.
 
 # Given a list of integers and a single sum value, return the first two values
 # (parse from the left please) in order of appearance that add up to form the sum.
@@ -30,38 +30,66 @@
 # NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements.
 # Be sure your code doesn't time out.
 
-def sum_pairs(ints, s)
-  results = {}
-  ints.each_with_index do |value1, index1|
-    ints.each_with_index do |value2, index2|
-      if index1 < index2
-        results[index2] = [value1, value2] if value1 + value2 == s
-      end
-    end
-  end
-  results.empty? ? nil : results.sort_by { |k, v| k }[0][1]
-end
-
-p sum_pairs([11, 3, 7, 5], 10)  == [3, 7]
+p sum_pairs([11, 3, 7, 5], 10) == [3, 7]
 p sum_pairs([4, 3, 2, 3, 4], 6) == [4, 2]
 p sum_pairs([0, 0, -2, 3], 2) == nil
 p sum_pairs([10, 5, 2, 3, 7, 5], 10) == [3, 7]
 
-# codewars solution:
 
-def sum_pairs(ints, sum)
-  seen = {}
-  ints.each do |integer|
-    return [sum - integer, integer] if seen.key?(sum - integer)
-    seen[integer] = true
-  end
-  nil
-end
 
-p sum_pairs([11, 3, 7, 5], 10)  == [3, 7]
-p sum_pairs([4, 3, 2, 3, 4], 6) == [4, 2]
-p sum_pairs([0, 0, -2, 3], 2) == nil
-p sum_pairs([10, 5, 2, 3, 7, 5], 10) == [3, 7]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sum_pairs(ints, s)
+#   results = {}
+#   ints.each_with_index do |value1, index1|
+#     ints.each_with_index do |value2, index2|
+#       if index1 < index2
+#         results[index2] = [value1, value2] if value1 + value2 == s
+#       end
+#     end
+#   end
+#   results.empty? ? nil : results.sort_by { |k, v| k }[0][1]
+# end
+
+# # codewars solution:
+
+# def sum_pairs(ints, sum)
+#   seen = {}
+#   ints.each do |integer|
+#     return [sum - integer, integer] if seen.key?(sum - integer)
+#     seen[integer] = true
+#   end
+#   nil
+# end
+
+# p sum_pairs([11, 3, 7, 5], 10)  == [3, 7]
+# p sum_pairs([4, 3, 2, 3, 4], 6) == [4, 2]
+# p sum_pairs([0, 0, -2, 3], 2) == nil
+# p sum_pairs([10, 5, 2, 3, 7, 5], 10) == [3, 7]
 
 
 
