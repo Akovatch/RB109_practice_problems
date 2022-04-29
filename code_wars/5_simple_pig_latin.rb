5765a87e1a6236304502eec1a1392a01e566241c
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-29 09:12:07 -0400

    Update 5_simple_pig_latin.rb

--- code_wars/5_simple_pig_latin.rb
+++ code_wars/5_simple_pig_latin.rb
@@ -1,28 +1,16 @@
 # 5 kyu
 
-# Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+# Move the first letter of each word to the end of it, then add "ay" to the end of the word.
+# Leave punctuation marks untouched.
 
-# Examples
-# pig_it('Pig latin is cool') # igPay atinlay siay oolcay
-# pig_it('Hello world !')     # elloHay orldway !
+PUNCTUATION = ['.', '!', '?']
 
-def pig_it(text)
-  words = text.split
-  words.map do |word|
-    new_word = word[1..-1] + word[0]
-    if word[-1] =~ /[a-zA-Z]/
-      new_word + 'ay'
-    else
-      word
-    end
+def pig_it(string)
+  string.split.map do |word|
+    PUNCTUATION.include?(word) ? word : word[1..-1] + word[0] + 'ay'
   end.join(' ')
 end
 
 p pig_it('Pig latin is cool') == 'igPay atinlay siay oolcay'
 p pig_it('Hello world !') == 'elloHay orldway !'
 
-# Codewars solution:
-
-def pig_it text
-  text.split(" ").map{ |word|["?", "!", "."].include?(word) ? word : word[1..-1] + word[0] + "ay" }.join(" ")
-end
