67dcd6074125407dc16fb7c8a8b5e5412fca2552
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-29 14:06:37 -0400

    Update 5_where_my_anagrams_at?.rb

--- code_wars/5_where_my_anagrams_at?.rb
+++ code_wars/5_where_my_anagrams_at?.rb
@@ -1,4 +1,4 @@
-# 5 kyu
+# 5 kyu - very easy for a level 5 kyu
 
 # What is an anagram? Well, two words are anagrams of each other if they
 # both contain the same letters. For example:
@@ -11,8 +11,8 @@
 
 # 'abba' & 'abca' == false
 
-# Write a function that will find all the anagrams of a word from a list.
-# You will be given two inputs a word and an array with words. You should
+# Write a function that will find all the anagrams of a word within an array.
+# You will be given two inputs: a word and an array with words. You should
 # return an array of all the anagrams or an empty array if there are none.
 
 # For example:
@@ -23,44 +23,12 @@
 
 # anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
 
+def anagrams(reference_word, words_array)
+  words_array.select do |word|
+    word.chars.sort == reference_word.chars.sort
+  end
+end
 
 p anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) == ['aabb', 'bbaa']
 p anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) == ['carer', 'racer']
 p anagrams('laser', ['lazing', 'lazy',  'lacer']) == []
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def anagrams(word, words)
-  words.select do |str|
-    str.chars.sort == word.chars.sort
-  end
-end
