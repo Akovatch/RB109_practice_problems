42b71d20620bc9c4415d78f42b815c0641f4e6b7
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-04-29 11:29:19 -0400

    Update 5_the_hashtag_generator.rb

--- code_wars/5_the_hashtag_generator.rb
+++ code_wars/5_the_hashtag_generator.rb
@@ -5,17 +5,14 @@
 
 # Here's the deal:
 
-# It must start with a hashtag (#).
-# All words must have their first letter capitalized.
-# If the final result is longer than 140 chars it must return false.
-# If the input or the result is an empty string it must return false.
-
-def generateHashtag(str)
-  hashtag = str.split.map { |word| word.capitalize }.join.gsub(' ', '').prepend('#')
-  (hashtag.length > 140  || hashtag == '#') ? false : hashtag
+def generate_hashtag(string)
+  return false if string == ''
+  result = '#' + string.split.map { |word| word.capitalize }.join('')
+  result.length > 140 ? false : result
 end
 
-p generateHashtag(" Hello there thanks for trying my Kata") == "#HelloThereThanksForTryingMyKata"
-p generateHashtag("    Hello     World   ") == "#HelloWorld"
-p generateHashtag("") == false
+p generate_hashtag(" Hello there thanks for trying my Kata") == "#HelloThereThanksForTryingMyKata"
+p generate_hashtag("    Hello     World   ") == "#HelloWorld"
+p generate_hashtag("") == false
+
 
