fbfdbd231f3e0b18f36d40bc02319d0598859d38
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-03 14:11:16 -0400

    Update v3-2.rb

--- video_problems/v3-2.rb
+++ video_problems/v3-2.rb
@@ -13,27 +13,25 @@
 
 # All given inputs are lowercase letters a-z.
 
-
-# input: an array of strings
-# output: string or empty string
+# input: array of strings (words)
+# output: common prefix (string) or an empty string
 # rules:
-  # It there is no common prefix, return an empty string.
-  # All given inputs are lowercase letters a-z.
+  # all letters are lowercase
 # algorithm:
-  # init variable: last_index = ''
-  # iterate through chars of the first word of array (each_with_index)
-    # check if all words have the same character at the index (all?)
-      # if not, break
-    # last_index reassigned to index each time
-    # slice first word from 0..last_index
+  # initialize results array
+  # shift first word out of array, assign to variable 'reference_word'
+  # iterate through chars of reference_word - for each char
+    # push char into results if it char is present at same index in all other words
+    # break if char is not present
+  # if results is empty, return empty string, otherwise, join and return results
 
 def common_prefix(array)
-  last_index = ''
-  array.first.chars.each_with_index do |char, index|
-    break if !array.all? { |word| word[index] == char }
-      last_index = index
+  results = []
+  reference_word = array.shift
+  reference_word.chars.each_with_index do |char, index|
+    array.all? { |word| word[index] == char } ? results << char : break
   end
-  last_index == '' ? '' : array.first[0..last_index]
+  results.empty? ? '' : results.join
 end
 
   p common_prefix(['flower', 'flow', 'flight']) == 'fl'
@@ -41,17 +39,3 @@ end
   p common_prefix(['interspecies', 'interstellar', 'interstate']) == 'inters'
   p common_prefix(['throne', 'dungeon']) == ''
   p common_prefix(['throve', 'throne']) == 'thro'
-
-# def common_prefix(array)
-#   results = []
-#   reference_word = array.shift.chars
-#   reference_word.each_with_index do |char, index|
-#     if array.all? { |word| word[index] == char }
-#        results << char
-#     else return results.join
-#     end
-#   end
-#   results.join
-# end
-
-
