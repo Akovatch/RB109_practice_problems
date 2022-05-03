bac270eab1b8d62f4e5323c1c8058f90a8bf3075
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-03 13:16:53 -0400

    Update v1-2.rb

--- video_problems/v1-2.rb
+++ video_problems/v1-2.rb
@@ -3,19 +3,6 @@
 # if a character occurs 3 times in all strings but not 4 times, you need to include that
 # character 3 times in the final answer.
 
-# input: an array of string words
-# output: an array of common string chars
-# rules:
-  # all letters are lowercase
-  # handle duplicates that are common
-# algorithm:
-  # initialize results array
-  # first word is a reference word (shift)
-  # iterate through chars of reference word - for each char
-    # if char is present in all words of array, push char into results array
-    # replace with an empty string in all words
-  # return results array
-
 def common_chars(array)
   results = []
   reference_word = array.shift
