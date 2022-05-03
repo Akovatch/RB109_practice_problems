fa0c560a27524fe0d200efabbf78320ee1d1ee29
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-03 12:59:46 -0400

    Update v1-1.rb

--- video_problems/v1-1.rb
+++ video_problems/v1-1.rb
@@ -2,16 +2,21 @@
 # of it and appending multiple copies of the substring together. You may assume the
 # given string consists of lowercase English letters only.
 
-def repeated_substring_pattern(str)
-  sub_strings = []
-  1.upto(str.length / 2) do |num|
-    if (str.length % num == 0)
-    sub_strings << str[0, num]
-    end
-  end
-  sub_strings.each do |sub_string|
-    multiplier = str.length / sub_string.length
-    return true if sub_string * multiplier == str
+# input: string
+# output: boolean
+# rules:
+  # input is non-empty
+  # input is lowercase letters only
+# algorithm:
+  # iterate from 0...string.length - for each index #
+    # get slice from string and multiply it by string length / slice length
+      # compare this to string
+      # if it is equal, return true
+  # false if no matches were found
+
+def repeated_substring_pattern(string)
+  (1..string.length / 2).each do |slice_size|
+    return true if string[0, slice_size] * (string.length / slice_size) == string
   end
   false
 end
@@ -21,7 +26,3 @@ p repeated_substring_pattern('aba') == false
 p repeated_substring_pattern('aabaaba') == false
 p repeated_substring_pattern('abaababaab') == true
 p repeated_substring_pattern('abcabcabcabcabc') == true
-
-
-
-
