27fa538df7d052b6bb6c01bfdb41cc49359545f6
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-03 13:00:05 -0400

    Update v1-1.rb

--- video_problems/v1-1.rb
+++ video_problems/v1-1.rb
@@ -2,18 +2,6 @@
 # of it and appending multiple copies of the substring together. You may assume the
 # given string consists of lowercase English letters only.
 
-# input: string
-# output: boolean
-# rules:
-  # input is non-empty
-  # input is lowercase letters only
-# algorithm:
-  # iterate from 0...string.length - for each index #
-    # get slice from string and multiply it by string length / slice length
-      # compare this to string
-      # if it is equal, return true
-  # false if no matches were found
-
 def repeated_substring_pattern(string)
   (1..string.length / 2).each do |slice_size|
     return true if string[0, slice_size] * (string.length / slice_size) == string
