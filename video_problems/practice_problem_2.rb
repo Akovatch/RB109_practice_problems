42c0c74eb59c5b704efa6135e06d3ab07db8fd1b
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-04 16:39:30 -0400

    Update practice_problem_2.rb

--- video_problems/practice_problem_2.rb
+++ video_problems/practice_problem_2.rb
@@ -3,27 +3,16 @@
 # numbers in the array. If the array contains fewer than 5
 # elements, the method should return nil.
 
-# input: array of integers
-# output: integer representing the minimum sum of 5 consecutive integers
-# rules:
-  # If the array contains fewer than 5 elements, the method should return nil.
-# algorithm:
-  # init results array
-  # If the array contains fewer than 5 elements, the method should return nil.
-  # interate from 0 upto array size - 5 (index)
-    # slice from index up 5 elements, push subarray into results array
-  # select the min subarray by sum, return the sum
-
 def minimum_sum(array)
-  results = []
   return nil if array.size < 5
-  0.upto(array.size - 5) do |index|
-    results << array[index, 5]
+  results = []
+  (0..array.size - 5).each do |index|
+    results << array[index, 5].sum
   end
-  results.min_by { |sub_array| sub_array.sum }.sum
+  results.min
 end
 
-p minimum_sum([1, 2, 3, 4]) == nil
-p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
-p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
-p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+  p minimum_sum([1, 2, 3, 4]) == nil
+  p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
+  p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
+  p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
