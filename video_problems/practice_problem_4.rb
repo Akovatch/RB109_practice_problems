caaa819d136d07b1c7284be3d0e73bf926ea8b48
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-04 17:09:41 -0400

    Update practice_problem_4.rb

--- video_problems/practice_problem_4.rb
+++ video_problems/practice_problem_4.rb
@@ -1,42 +1,16 @@
 # Write a method that takes an array of integers and returns the
 # two numbers that are closest together in value.
 
-
-
-
-
-
+def closest_numbers(array)
+  results = array.combination(2).to_a
+  results.min_by { |sub_array| (sub_array[0] - sub_array[1]).abs }
+end
 
 p closest_numbers([5, 25, 15, 11, 20]) == [15, 11]
 p closest_numbers([19, 25, 32, 4, 27, 16]) == [25, 27]
 p closest_numbers([12, 7, 17]) == [12, 7]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# alt solution:
 
 # def closest_numbers(array)
 #   results = []
