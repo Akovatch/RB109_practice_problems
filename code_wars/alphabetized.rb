282f0733d0e6209f23166f571c1362c61fba4a30
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-04 18:56:00 -0400

    Update alphabetized.rb

--- code_wars/alphabetized.rb
+++ code_wars/alphabetized.rb
@@ -1,4 +1,4 @@
-# 6 kyu
+# 6 kyu 
 
 # Re-order the characters of a string, so that they are concatenated into a
 # new string in "case-insensitively-alphabetical-order-of-appearance" order.
@@ -10,17 +10,37 @@
 # Example:
 # alphabetized("The Holy Bible") # "BbeehHilloTy"
 
-ALPHABET = ('a'..'z').to_a
+# input: string
+# output: new string in "case-insensitively-alphabetical-order-of-appearance" order
+# rules:
+  # white space and punctuation are removed
+  # input is only letters and punctuation - no digits
+# algorithm:
+  # remove punctuation and whitespace
+    # push into results if it matches regex
+  # sort by value lowercase, tie breaker is index within the original string
 
 def alphabetized(string)
-  results = []
-  ALPHABET.each do |letter|
-    string.chars.each { |char| results << char if char.downcase == letter }
-  end
-  results.join
+  only_letters = []
+  string.chars.each { |char| only_letters << char if char =~ /[a-zA-Z]/ }
+  only_letters.sort_by { |char| [char.downcase, string.index(char)] }.join
 end
 
 p alphabetized("The Holy Bible") == "BbeehHilloTy"
+
+# alt solution:
+
+# ALPHABET = ('a'..'z').to_a
+
+# def alphabetized(string)
+#   results = []
+#   ALPHABET.each do |letter|
+#     string.chars.each { |char| results << char if char.downcase == letter }
+#   end
+#   results.join
+# end
+
+# p alphabetized("The Holy Bible") == "BbeehHilloTy"
 
 # because we iterate over the alphabet, all chars will be sorted alphabetically
 # by order of appearance in the string.
