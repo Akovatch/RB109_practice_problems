c017a54b3c2d5f661a64a6315693a418afa01563
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-05 19:10:40 -0400

    Update dubstep.rb

--- code_wars/dubstep.rb
+++ code_wars/dubstep.rb
@@ -29,11 +29,10 @@ song_decoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")  =>  WE ARE TH
 
 =end
 
-def song_decoder(song)
-  p song.gsub('WUB', ' ').split.join(' ')
+def song_decoder(string)
+  string.gsub('WUB', ' ').split.join(' ')
 end
 
 p song_decoder("AWUBBWUBC") == "A B C"
 p song_decoder("AWUBWUBWUBBWUBWUBWUBC") == "A B C"
 p song_decoder("WUBAWUBBWUBCWUB") == "A B C"
-
