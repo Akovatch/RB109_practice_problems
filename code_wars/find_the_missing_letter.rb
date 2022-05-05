c4a82bb1df91f7fbfcf04c94fddf8ff82f2beba5
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-05 19:49:36 -0400

    Update find_the_missing_letter.rb

--- code_wars/find_the_missing_letter.rb
+++ code_wars/find_the_missing_letter.rb
@@ -1,12 +1,14 @@
 =begin
 
-6 kyu
+6 kyu - note: try using the next method
 
 #Find the missing letter
 
-Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+Write a method that takes an array of consecutive (increasing) letters as input and that
+returns the missing letter in the array.
 
-You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+You will always get an valid array. And it will be always exactly one letter be missing.
+The length of the array will always be at least 2.
 The array will always contain letters in only one case.
 
 Example:
@@ -19,12 +21,9 @@ Example:
 
 =end
 
-# Try using the 'next' method next time...
-
-
 def find_missing_letter(array)
-  1.upto(array.size - 1) do |index|
-    return (array[index - 1].ord + 1).chr if array[index].ord != array[index - 1].ord + 1
+  array.each do |char|
+    return char.next if !array.include?(char.next)
   end
 end
 
@@ -33,14 +32,3 @@ p find_missing_letter(["O","Q","R","S"]) == "P"
 p find_missing_letter(["b","d"]) == "c"
 p find_missing_letter(["a","b","d"]) == "c"
 p find_missing_letter(["b","d","e"]) == "c"
-
-# Codewars solution:
-
-# def find_missing_letter(arr)
-#   last_letter = arr[0]
-#   1.upto(arr.size - 1) do |index|
-#     arr[index] == last_letter.next ? last_letter = arr[index] : break
-#   end
-#   last_letter.next
-# end
-
