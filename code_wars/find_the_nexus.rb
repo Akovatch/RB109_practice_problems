5a47d4fc1c98c093324b29902124c9db8a36f799
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-05 19:49:45 -0400

    Update find_the_nexus.rb

--- code_wars/find_the_nexus.rb
+++ code_wars/find_the_nexus.rb
@@ -1,12 +1,12 @@
 =begin
 
-6 kyu - Tricky (use sorting methods, not pushing to hash)
+6 kyu - realtively easy
 
 Not to brag, but I recently became the nexus of the Codewars universe! My honor and my rank
 were the same number. I cried a little.
 
 Complete the method that takes a hash list of users, and find the
-nexus: the user whose rank is the closest is equal to his honor. Return the rank of this user.
+nexus: the user whose rank is the closest to his honor. Return the rank of this user.
 For each user, the key is the rank and the value is the honor.
 
 If nobody has an exact rank/honor match, return the rank of the user who comes closest. If t
@@ -24,9 +24,34 @@ users = {  1  =>  93,
 =end
 
 def nexus(hash)
-  hash.sort.sort_by { |array| (array[0] - array[1]).abs }.first.first
+  hash.min_by { |key, value| (key - value).abs }[0]
 end
 
 p nexus({1 => 3, 3 => 3, 5 => 1}) == 3
 p nexus({1 => 10, 2 => 6, 3 => 4, 5 => 1}) == 3
 p nexus({1 => 10, 2 => 3, 3 => 4, 5 => 1}) == 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def nexus(hash)
+#   hash.sort.sort_by { |array| (array[0] - array[1]).abs }.first.first
+# end
