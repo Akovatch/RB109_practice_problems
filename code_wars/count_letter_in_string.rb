f6976dbe72862b3bbe1b8b643097df1548353f5a
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-05 16:49:08 -0400

    Update count_letter_in_string.rb

--- code_wars/count_letter_in_string.rb
+++ code_wars/count_letter_in_string.rb
@@ -7,23 +7,15 @@ In this kata, you've to count lowercase letters in a given string and return the
 
 =end
 
-ALPHABET = ('a'..'z').to_a
-
 def letter_count(string)
   results = {}
-  ALPHABET.each do |letter|
-    count = string.count(letter)
-    results[letter.to_sym] = count if count > 0
+  string.chars.uniq.sort.each do |char|
+    count = string.count(char)
+    results[char.to_sym] = count
   end
   results
 end
 
 p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
-
-# Codewars solution:
-
-def letter_count(str)
-  str.chars.each_with_object(Hash.new(0)) { |letter, hash| hash[letter.to_sym] += 1 }
-end
 
 
