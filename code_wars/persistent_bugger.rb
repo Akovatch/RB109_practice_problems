a29124046a57be6fa72f3bb2f5d8fa7fe4e71880
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-06 19:22:10 -0400

    Update persistent_bugger.rb

--- code_wars/persistent_bugger.rb
+++ code_wars/persistent_bugger.rb
@@ -11,19 +11,6 @@
 # 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 # 4 --> 0 (because 4 is already a one-digit number)
 
-# input: integer
-# output: integer representing number of times we multiply the digits together
-# rules:
-  # input will be positive
-  # return 0 if you don't multiply it
-# algorithm:
-  # init counter
-  # start a loop
-    # break if the number only has one digit
-    # seperate and multiply digits
-    # increment the counter
-  # return counter
-
 def persistence(num)
   counter = 0
   loop do
