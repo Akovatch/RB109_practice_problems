292bb36986a78390b04cafca3018fecbc2d32708
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-06 18:59:20 -0400

    Update multiples_of_3_or_5.rb

--- code_wars/multiples_of_3_or_5.rb
+++ code_wars/multiples_of_3_or_5.rb
@@ -4,40 +4,13 @@
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 # Finish the solution so that it returns the sum of all the multiples of
-# 3 or 5 below the number passed in. Additionally,
+# 3 or 5 below the number passed in.
 
 # Note: If the number is a multiple of both 3 and 5, only count it once.
 
+def solution(num)
+  (1...num).to_a.select { |num| num % 3 == 0 || num % 5 == 0 }.sum
+end
+
 p solution(10) == 23
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def solution(number)
-#   (1...number).select do |num|
-#     num % 3 == 0 || num % 5 == 0
-#   end.sum
-# end
 
