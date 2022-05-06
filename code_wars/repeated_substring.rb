3d8c432c044db26c3060cfa471aaba8b0fd50faf
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-06 19:53:51 -0400

    Update repeated_substring.rb

--- code_wars/repeated_substring.rb
+++ code_wars/repeated_substring.rb
@@ -23,32 +23,14 @@ because for this string "abcde" the minimum substring t, such that s is t repeat
 
 =end
 
-# input: string
-# output: an array containing a substring and an integer
-# rules: non-empty
-# algorithm:
-  # initialize a results hash
-  # generate all substrings
-    # nested upto iterations
-    # generate all substrings
-    # iterate over substrings
-      # if substring can be multiplied to create string, push substring (key) into a results hash along with the multiplier (value)
-        # substring * number from 1 to string length / 2
-  # select the pair with the smallest key by character length
-
-def f(str)
-  substrings = []
-  results = {}
-  0.upto(str.length) do |index|
-    substrings << str[0..index]
-  end
-  substrings.each do |substring|
-    (1..str.length).each do |multiplier|
-    return [substring, multiplier] if substring * multiplier == str
-    end
+def f(string)
+  multiplier = 0
+  (0...string.length).each do |index|
+    multiplier = string.length / string[0..index].length
+    return [string[0..index], multiplier] if string[0..index] * multiplier == string
   end
 end
 
-p f("ababab") == ["ab", 3]
-p f("abcde") == ["abcde", 1]
-p f("a") == ["a", 1]
+  p f("ababab") == ["ab", 3]
+  p f("abcde") == ["abcde", 1]
+  p f("a") == ["a", 1]
