5bc4d9cc6058ee4e217fa2574d1f341a4d5c391c
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-06 17:04:13 -0400

    Update is_integer_array.rb

--- code_wars/is_integer_array.rb
+++ code_wars/is_integer_array.rb
@@ -12,16 +12,6 @@ returns false / False for every other input. (Check test cases to see the variet
 
 =end
 
-# input: an array (or a multitide of other types)
-# output: boolean
-# rules:
-  # returns true / True if every element in an array is an integer or a float with no decimals.
-  # returns true / True if array is empty.
-  # returns false / False for every other input. (Check test cases to see the variety of inputs and edge cases)
-# algorithm:
-  # iterate over elements of array - for each element (#all)
-    # num.to_i.to_f == num
-
 def is_int_array(array)
   return false if !array.is_a?(Array)
   array.all? { |num| num.to_i.to_f == num }
