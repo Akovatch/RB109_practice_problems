15d982d63fb82ce077c13ce622925c027631a0b9
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-07 11:09:45 -0400

    Update split_strings.rb

--- code_wars/split_strings.rb
+++ code_wars/split_strings.rb
@@ -8,28 +8,14 @@ missing second character of the final pair with an underscore ('_').
 
 =end
 
-# input: string
-# output: an array of 2-char strings
-# rules: add _ to last char if it is a single char
-# algorithm:
-  # init results array
-  # init coutner
-  # loop
-    # slice out two char substrings and push to results array
-      # ex. string[counter..counter + 1]
-    # counter += 2
-  # if last string in results array has only one char, add _
-
 def solution(string)
-  return [] if string == ''
-  results = []
-  string.chars.each_slice(2) { |slice| results << slice.join }
-  results[-1].length == 1 ? results[-1] << '_' : results
-  results
+  string.chars.each_slice(2).map do |slice|
+    slice.size < 2 ? slice.first + '_' : slice.join
+  end
 end
 
-p solution('abc') == ['ab', 'c_']
-p solution('abcdef') == ['ab', 'cd', 'ef']
-p solution("abcdef") == ["ab", "cd", "ef"]
-p solution("abcdefg") == ["ab", "cd", "ef", "g_"]
-p solution("") == []
+  p solution('abc') == ['ab', 'c_']
+  p solution('abcdef') == ['ab', 'cd', 'ef']
+  p solution("abcdef") == ["ab", "cd", "ef"]
+  p solution("abcdefg") == ["ab", "cd", "ef", "g_"]
+  p solution("") == []
