39086f9f9ea6402ac50a595aa6370ff4ea7afc5b
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-12 14:15:07 -0400

    Update 5_string_incrementer.rb

--- code_wars/5_string_incrementer.rb
+++ code_wars/5_string_incrementer.rb
@@ -16,21 +16,9 @@
 
 # Attention: If the number has leading zeros the amount of digits should be considered.
 
-# input: string
-# output: string
-# rules:
-  # If the string already ends with a number, the number should be incremented by 1.
-  # If the string does not end with a number, the number 1 should be appended to the new string.
-  # If the number has leading zeros the amount of digits should be considered.
-# algorithm:
-  # partition letters from string digits (assign to variables, letters and numbers, representing arrays)
-    # join numbers and get next number
-    # join letters, add numbers results
-      # if numbers is empty, add '1' to the end of letters (joined)
-
 def increment_string(string)
-  letters, numbers = string.chars.partition { |char| char =~ /[a-zA-Z]/ }
-  numbers.empty? ? letters.join + '1' : letters.join + numbers.join.next
+  numbers, letters = string.chars.partition { |char| char =~ /[0-9]/ }
+  numbers.empty? ?  letters.join + '1' : letters.join + numbers.join.next
 end
 
 p increment_string("foo") == "foo1"
@@ -39,3 +27,4 @@ p increment_string("foobar1") == "foobar2"
 p increment_string("foobar00") == "foobar01"
 p increment_string("foobar99") == "foobar100"
 p increment_string("") == "1"
+
