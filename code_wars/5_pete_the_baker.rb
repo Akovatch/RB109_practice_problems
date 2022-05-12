61d5487b10d6bc0860ca21b27fd03bf9b674e88f
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-12 09:11:56 -0400

    Update 5_pete_the_baker.rb

--- code_wars/5_pete_the_baker.rb
+++ code_wars/5_pete_the_baker.rb
@@ -12,7 +12,7 @@ def cakes(recipe, ingredients)
   results = []
   recipe.each do |key, value|
     return 0 if !ingredients.key?(key)
-    results << ingredients[key] / value
+  results << ingredients[key] / value
   end
   results.min
 end
@@ -21,4 +21,3 @@ p cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, m
 p cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}) == 0
 p cakes({"cream" => 200, "flour" => 300, "sugar" => 150, "milk" => 100, "oil" => 100},{"sugar" => 1700, "flour" => 20000, "milk" =>20000, "oil" => 30000, "cream" => 5000}) == 11
 p cakes({"cream" => 1, "flour" =>3 , "sugar" => 1, "milk" => 1, "oil" => 1, "eggs" => 1},{"sugar" => 1, "eggs" => 1, "flour" => 3, "cream" => 1, "oil" => 1, "milk" => 1}) == 1
-
