c01790828dd40b2d9bebdaaeae686bb0abd87808
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-12 12:18:59 -0400

    Update 5_simple_pig_latin.rb

--- code_wars/5_simple_pig_latin.rb
+++ code_wars/5_simple_pig_latin.rb
@@ -3,12 +3,20 @@
 # Move the first letter of each word to the end of it, then add "ay" to the end of the word.
 # Leave punctuation marks untouched.
 
-PUNCTUATION = ['.', '!', '?']
+# input: string
+# output: string in pig latin
+# rules:
+  # assuming no empty or invalid inputs
+# algorithm:
+  # create a constant array of punctutaion marks
+  # iterate over words - transform each word
+  # if its a punc mark, return punc mark
+  # add first char to end of word from 1..-1 + ay
+
+PUNCTUATION = ['.', '?', '!']
 
 def pig_it(string)
-  string.split.map do |word|
-    PUNCTUATION.include?(word) ? word : word[1..-1] + word[0] + 'ay'
-  end.join(' ')
+  string.split.map { |word| PUNCTUATION.include?(word) ? word : word[1..-1] + word[0] + 'ay' }.join(' ')
 end
 
 p pig_it('Pig latin is cool') == 'igPay atinlay siay oolcay'
